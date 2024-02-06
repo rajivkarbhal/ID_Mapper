50a386a48a1e6f76c4139f859379cce6b7f25568
Rajiv Karbhal <rajivkarbhal@gmail.com>
2024-02-06 10:41:14 +0100

    Rajiv:	Line added in script

--- fetch_try_2.py
+++ fetch_try_2.py
@@ -7,6 +7,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
 # Its demo change file
+# Second change
 
 class gene:
     def __init__(self,unurl,strurl,ensurl,ucscurl,omimurl,snpurl,varurl,hlmurl,cddurl,bgrdurl,minturl,stringurl,cprturl,inturl,keggurl,gobpurl,goccurl,gomfurl):
